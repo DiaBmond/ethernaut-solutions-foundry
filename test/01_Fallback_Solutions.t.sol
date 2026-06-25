249a92bf657f726305bec94483dbb814de284d40
kodchamon  boonjan <117715922+DiaBmond@users.noreply.github.com>
2026-06-25 18:09:48 +0700

    restructure project

--- test/01_Fallback_Solutions.t.sol
+++ test/01_Fallback_Solutions.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
-import {Fallback} from "../src/01_Fallback.sol";
+import {Fallback} from "../src/01_Fallback/Fallback.sol";
 
 contract Fallback_Solutions is Test {
     // Changed the name from targetContract to fallbackContract
--- test/02_Fallout_Solutions.t.sol
+++ test/02_Fallout_Solutions.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
-import {Fallout} from "../src/02_ Fallout.sol";
+import {Fallout} from "../src/02_Fallout/Fallout.sol";
 
 contract Fallback_Solutions is Test {
     Fallout falloutContract;
